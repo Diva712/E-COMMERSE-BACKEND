8106f92c07dfc53f63c8f938a03f9bbfa01e253a
Diva gupta <divagupta712@gmail.com>
2024-07-26 22:19:46 +0530

    get single product

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,5 +1,6 @@
 const ProductModel = require("../models/ProductModel");
 
+//get all product
 const getAllProduct = async (req, res) => {
   try {
     const products = await ProductModel.find({});
@@ -18,4 +19,41 @@ const getAllProduct = async (req, res) => {
   }
 }
 
-module.exports = { getAllProduct };
+
+//get single product
+const getSingleProduct = async (req, res) => {
+  try {
+
+    const product = await ProductModel.findById(req.params.id);
+    if (!product) {
+      return res.status(500).send({
+        success: false,
+        message: 'Product not found !!',
+        error,
+      })
+    }
+
+    res.status(200).send({
+      success: true,
+      message: "Product Found",
+      product,
+    })
+
+  } catch (error) {
+    console.log(error);
+    if (error.name === "CastError") {
+      res.status(500).send({
+        success: false,
+        message: 'Invalid Id',
+
+      })
+    }
+    res.status(500).send({
+      success: false,
+      message: 'Error in Get single product Api',
+      error,
+    })
+  }
+}
+
+module.exports = { getAllProduct, getSingleProduct };
--- routers/ProductRoutes.js
+++ routers/ProductRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const { getAllProduct } = require('../controllers/ProductController');
+const { getAllProduct, getSingleProduct } = require('../controllers/ProductController');
 const router = express.Router();
 
 
 router.route('/get-all').get(getAllProduct);
+router.route('/:id').get(getSingleProduct);
+
+
+
 
 module.exports = router;
