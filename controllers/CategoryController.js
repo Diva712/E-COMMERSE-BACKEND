fd7e90c93bb3eae45a093293668f636019791d9d
Diva gupta <divagupta712@gmail.com>
2024-07-27 01:06:58 +0530

    category update

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -88,9 +88,58 @@ const deleteCategory = async (req, res) => {
   }
 }
 
+//updateCategory
+const updateCategory = async (req, res) => {
+  try {
+    // find category
+    const category = await categoryModel.findById(req.params.id);
+    //validation
+    if (!category) {
+      return res.status(404).send({
+        success: false,
+        message: "Category not found",
+      });
+    }
+    // get new cat
+    const { updatedCategory } = req.body;
+    // find product with this category id
+    const products = await productModel.find({ category: category._id });
+    // update producty category
+    for (let i = 0; i < products.length; i++) {
+      const product = products[i];
+      product.category = updatedCategory;
+      await product.save();
+    }
+    if (updatedCategory) category.category = updatedCategory;
+
+    // save
+    await category.save();
+    res.status(200).send({
+      success: true,
+      message: "Catgeory Updated Successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    // cast error ||  OBJECT ID
+    if (error.name === "CastError") {
+      return res.status(500).send({
+        success: false,
+        message: "Invalid Id",
+      });
+    }
+    res.status(500).send({
+      success: false,
+      message: "Error In UPDATE CATEGPORY API",
+      error,
+    });
+  }
+
+}
+
 module.exports = {
   createCategory,
   getAllCategory,
-  deleteCategory
+  deleteCategory,
+  updateCategory
 
 };
--- routers/CategoryRoutes.js
+++ routers/CategoryRoutes.js
@@ -1,11 +1,12 @@
 
 const express = require('express');
-const { createCategory, getAllCategory, deleteCategory } = require('../controllers/CategoryController');
+const { createCategory, getAllCategory, deleteCategory, updateCategory } = require('../controllers/CategoryController');
 const router = express.Router();
 
 router.route('/create').post(createCategory)
 router.route('/').get(getAllCategory);
 router.route('/delete/:id').delete(deleteCategory);
+router.route('/update/:id').patch(updateCategory);
 
 
 
