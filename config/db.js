d0e2f7b05ced346ac357d5629d845938eaae5315
Diva gupta <divagupta712@gmail.com>
2024-07-30 22:49:18 +0530

    mongoose uri changes

--- config/db.js
+++ config/db.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
+  const uri = process.env.DB_URL;
+  if (!uri) {
+    console.error('MongoDB URI is not defined. Please set the MONGO_URI environment variable.');
+    process.exit(1);
+  }
   try {
 
-    const instance = await mongoose.connect(process.env.DB_URL)
-    console.log(`Mongodb connected : ${instance.connection.host}`)
+    const instance = await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+      .then(() => console.log('MongoDB connected'))
+      .catch(err => console.error('MongoDB connection error:', err));
 
   } catch (error) {
     console.log(`MongoDb Error : ${error}`)
